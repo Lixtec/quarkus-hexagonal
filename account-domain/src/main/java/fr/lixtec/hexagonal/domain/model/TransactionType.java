010ea933415f431f0b368faa484a5bea55977d1d
Ludovic TERRAL <ludovic.terral@lixtec.fr>
2023-04-26 23:25:07 +0200

    ajout de la partie domain

---
+++ account-domain/src/main/java/fr/lixtec/hexagonal/domain/model/TransactionType.java
@@ -0,0 +1,12 @@
+package fr.lixtec.hexagonal.domain.model;
+
+/**
+ * Cette énumération regroupe tous les types de transactions
+ * 
+ * @author Ludovic.terral
+ */
+public enum TransactionType 
+{
+    Debit,
+    Credit
+}
